6269211abfb930b81e32426d4ad5cf92dd6f5788
Diana Denysyuk <didenysyuk@gmail.com>
2018-11-10 17:36:36 +0200

    Add logo

--- index.php
+++ index.php
@@ -1,1 +1,24 @@
-Home page
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home</title>
+		<link rel="stylesheet" type="text/css" href="/assets/style.css">
+	</head>
+	<body>
+		<header>
+			<div class="logo-menu">
+				<img class="logo"
+				src="/assets/logo_2018-11-10/logo.png" 
+				srcset="/assets/logo_2018-11-10/logo@2x.png 2x, 
+						/assets/logo_2018-11-10/logo@3x.png 3x">
+				</div>
+		</header>
+		<div class="main-content">
+			
+		</div>
+		<footer>
+			
+		</footer>
+	</body>
+</html>
