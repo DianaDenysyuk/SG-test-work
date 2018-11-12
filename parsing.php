c42d4c9cd31eacd18dcc00caa414b9e5c72daf5d
Diana Denysyuk <didenysyuk@gmail.com>
2018-11-12 23:52:21 +0200

    Add Validation to Parsing page

--- parsing.php
+++ parsing.php
@@ -12,6 +12,7 @@
 		</p>
 		<div class="metatag-table"></div>
 		<script src="/assets/jquery.js"></script>
+		<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 		<script src="/assets/main.js"></script>
 	</body>
 </html>
